c617c3b1333a62b815b69e38f7d6c775b3047cc1
Glenn Matthews <glenn.a.matthews@gmail.com>
2012-04-25 15:50:46 -0700

    Finish sign in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(params[:user])
     if @user.save
+      sign_in @user
       flash[:success] = "Welcome to CultureDab....Make your profile unique by editing your page"
       redirect_to @user
     else
